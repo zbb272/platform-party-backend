fa03ad2b757edcb058f12de79123e20819850339
github username <github email address>
2019-04-18 10:07:56 -0400

    got serializer working properly for games, levels, blocks

--- app/serializers/level_serializer.rb
+++ app/serializers/level_serializer.rb
@@ -1,5 +1,5 @@
 class LevelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :startPositionX, :startPositionY, :high_score
+  attributes :id, :name, :startPositionX, :startPositionY, :high_score, :blocks
   has_many :blocks
   belongs_to :game
 end
